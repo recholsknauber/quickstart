0a5eb8b8c198139295c7ad72db93b4bf1ff730ae
Ryan Knauber <ryanknauber@yahoo.com>
2023-10-08 15:49:52 -0700

    <(-.0<)

--- src/main/quickstart/balls.cljs
+++ src/main/quickstart/balls.cljs
@@ -19,24 +19,35 @@
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
-  (q/background 0 0 0)
+  (q/background 0)
   ; Set circle color.
-  (q/fill (:color state) 255 255)
+  (q/fill ;(:color (if (> (+ 100 state) 255) 255 (+ 100 state)))
+          (:color state) 255 255
+          )
   ; Calculate x and y coordinates of the circle.
   (let [angle (:angle state)
-        x (* 50 (q/cos angle))
-        y (* 50 (q/sin angle))]
+        x (* 35 (q/cos angle))
+        y (* 35 (q/sin angle))]
     ; Move origin point to the center of the sketch.
     (q/with-translation [(/ (q/width) 2)
                          (/ (q/height) 2)]
       ; Draw the circle.
-      (q/ellipse x y (+ 25 (rand-int 100)) (+ 25 (rand-int 100))))))
+      (q/ellipse x y
+                 ;(* (q/width) (/ 100.0 (+ 1 (rand-int 100))))
+                 ;(* (q/height) (/ 100.0 (+ 1 (rand-int 100))))
+                 (* (* 0.64 (q/height)) (/ (+ 30 (rand-int 61)) 100.0))
+                 (* (* 0.64 (q/height)) (/ (+ 30 (rand-int 61)) 100.0))
+                 ;(+ 30 (rand-int 71))
+                 ;(+ 30 (rand-int 71))
+                 ))))
 
 ; this function is called in home view
-(defn ^:export run-sketch [host]
+(defn ^:export run-sketch [host sc_width sc_height]
   (q/sketch
     :host host
-    :size [250 250]
+    :size [(* sc_height 0.2)
+           (* sc_height 0.2)]
+    ;; :size [250 250]
     ; setup function called only once, during sketch initialization.
     :setup setup
     ; update-state is called on each iteration before draw-state.
